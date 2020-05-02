b0eac8b694fa091a52854111ac39d9a99ab3a96b
vksxkfkttk <vksxkfkttk@gmail.com>
2020-05-02 13:05:44 +0900

    remove console.log

--- clock.js
+++ clock.js
@@ -6,7 +6,6 @@ function getTime(){
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    // console.log(date);
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
--- greetings.js
+++ greetings.js
@@ -12,7 +12,6 @@ function saveName(text){
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = input.value;
-    // console.log(currentValue);
     paintGreeting(currentValue);
     saveName(currentValue);
 }
